51b3c1b92cb0980f10f48bcc6adf842465ca0d4f
aaronrc <aaronrc2022@gmail.com>
2026-03-21 17:24:17 -0500

    solucion a conflictos almacen

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,30 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/clear-cache', function () {
+    // Limpiar cache de config
+    Artisan::call('config:clear');
+
+    // Limpiar cache de rutas
+    Artisan::call('route:clear');
+
+    // Limpiar cache de vistas
+    Artisan::call('view:clear');
+
+    // Limpiar cache de aplicación
+    Artisan::call('cache:clear');
+
+    return "Cache limpiada correctamente ✅";
+});
+
+
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return "Storage link creado correctamente ✅";
+});
 
 require __DIR__ . "/Auth/LoginRoute.php";
 require __DIR__ ."/Administracion/EmpleadoRoute.php";

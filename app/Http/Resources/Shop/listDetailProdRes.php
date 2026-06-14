1dfd3e567e6f92268d3c5ff891b054d0d5e53cde
aaronrc <aaronrc2022@gmail.com>
2026-06-13 19:44:38 -0500

    ajustes en show de inventario

--- app/Http/Resources/Shop/listDetailProdRes.php
+++ app/Http/Resources/Shop/listDetailProdRes.php
@@ -16,6 +16,10 @@ class listDetailProdRes extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        if (is_null($this->resource)) {
+            return [];
+        }
+
         return [
             'id' => $this->id ?? null,
             'stock' => $this->stock ?? null,
